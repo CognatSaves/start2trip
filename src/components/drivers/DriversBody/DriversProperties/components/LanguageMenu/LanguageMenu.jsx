a4907d2192eb87a17bcf24083735f6a81d663207
CognatSaves <cognatheptanirsson@gmail.com>
2019-02-28 11:03:02 +0300

    28/02/19 part 3

--- src/components/drivers/DriversBody/DriversProperties/components/LanguageMenu/LanguageMenu.jsx
+++ src/components/drivers/DriversBody/DriversProperties/components/LanguageMenu/LanguageMenu.jsx
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 import './LanguageMenu.css'
 
-export default class LanguageMenu extends React.Component{
+
+export default class LanguageMenu extends React.Component {
     constructor(props) {
-        super(props);      
-      }
-    render(){        
-        if(this.props.isVisible){
-            return(
+        super(props);
+    }
+    render() {
+        if (this.props.isVisible) {
+            return (
                 <div className="drivers_properties_languageMenu" >
-                    {this.props.languages.map((element,index)=>
-                        <div className="languageMenu_element" onClick={()=>this.props.languageValueChoose(element.languageName)}>                               
-                            <div className="languageMenu_element_flag" />
-                                <div className="textBlock_value">
-                                    {element.languageName}
-                                </div>                                 
+                    {this.props.languages.map((element, index) =>
+                        <div className="languageMenu_element" onClick={() => this.props.languageValueChoose(element.languageName)}>
+                            <img src={element.icon} alt="icon" width="15px" height="15px" />
+                            <div className="textBlock_value">
+                                {element.languageName}
+                            </div>
                         </div>
                     )}
                 </div>
             )
         }
-        else{
-            return(
+        else {
+            return (
                 <React.Fragment></React.Fragment>
             )
         }
