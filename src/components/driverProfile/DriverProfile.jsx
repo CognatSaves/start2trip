c05b1cde2330609c59fc9b79da931dd76ed68a5e
CognatSaves <cognatheptanirsson@gmail.com>
2019-03-13 09:08:28 +0300

    13.03.19/lev/1

--- src/components/driverProfile/DriverProfile.jsx
+++ src/components/driverProfile/DriverProfile.jsx
@@ -38,14 +38,14 @@ const DriverAdaptedRoute = (props)=>
     const {element, date,cities,travelLength,travelTime,goToDrivers, changeTravelVisibility} = props;
     let isVisibleArray = Array(cities.length).fill("visible");
       isVisibleArray[isVisibleArray.length-1]="hidden";
-      let routeElementWidth=100/(isVisibleArray.length+1);
-      routeElementWidth=routeElementWidth+"%";
+      /*let routeElementWidth=100/(isVisibleArray.length+1);
+      routeElementWidth=routeElementWidth+"%";*/
       
         return (
-          <div className = "drivers_route">
-            <div className="route_date" style={{margin: 0}}>
+          <div className = "drivers_route col-12 d-flex flex-column">
+            <div className="route_date d-flex flex-row" style={{margin: 0}}>
               <div className="route_date_text">Ваш индивидуальный маршрут на: {date}</div>
-              <div className="route_change" to="/drivers" onClick={()=>goToDrivers()}>                              
+              <div className="d-flex flex-row" to="/drivers" onClick={()=>goToDrivers()}>                              
                 <div className="route_change_text">Изменить маршрут</div>
                 <div className="route_change_emblem">
                   <img src={changeElement} width="100%" height="100%" alt="change"></img>
@@ -53,13 +53,13 @@ const DriverAdaptedRoute = (props)=>
               </div>
             </div>
             <div className="driversAdaptedRoute_routeBlock" >
-                <div style={{display: "flex", flexDirection: "column", width: "60%", marginRight: "auto", marginTop: "2%"}}>
-                    <div className="route_show" >
+                <div class="col-8" style={{display: "flex", flexDirection: "column", marginRight: "auto", padding: 0, marginTop: "2%"}}>
+                    <div className="route_show d-flex flex-row" >
                     {cities.map((element, index) =>
-                    <div className="route_show_element" style={{width: routeElementWidth}}>
-                        <img src={pointIcon} height="100%" width="25%" alt={"icon"+index}/>
-                        <div className="route_show_text" >{cities[index]}</div>
-                        <div className="route_show_line" style={{visibility: isVisibleArray[index]}}></div>
+                    <div className="route_show_element driverAdaptedRoute_show_element" style={{/*width: routeElementWidth*/}}>
+                        <img src={pointIcon} style={{marginTop: "4px"}} height="36px" width="auto" alt={"icon"+index}/>
+                        <div className="route_show_text driverAdaptedRoute_show_text" >{cities[index]}</div>
+                        <div className="route_show_line driverAdaptedRoute_show_line" style={{visibility: isVisibleArray[index]}}></div>
                     </div>
                     )}
                     </div>
@@ -70,7 +70,7 @@ const DriverAdaptedRoute = (props)=>
                     <div className="route_comment">*Возврат в точку отправления в этот же день бесплатно</div>
                     </div> 
                 </div>
-                <div style={{display: "flex", flexDirection: "column", width: "30%", marginLeft: "auto", marginRight: "10%"}}>
+                <div class="col-4" style={{display: "flex", flexDirection: "column", marginLeft: "auto"}}>
                     <div className="driversAdaptedRoute_price">{"$"+element.price}</div>
                     <button className="driversAdaptedRoute_sendRequest" onClick={()=>changeTravelVisibility('block')}>ЗАБРОНИРОВАТЬ ПОЕЗДКУ</button>
                     <div className="driversAdaptedRoute_requestCommentary">Стоимость окончательная. Топливо включено</div>
@@ -82,25 +82,20 @@ const DriverAdaptedRoute = (props)=>
 }
 const DriverInfo = (props) =>{
     const {element, changeCar, carImageNumber, carImages} = props;
-    console.log("DriverInfo creation");
-    console.log(changeCar);
+
     let arrowLeft = "<";
     let arrowRight=">";
     let driverInfo = "Несмотря на дым и грохот, обучиться стрельбы из гладкоствольного дульнозарядного мушкета может любой, если только он не клинический идиот. Как только человек осваивает основой прием заряжания, остается только направить на врага дуло и спустить курок, остальное в руках Божьих!";
     
-    console.log("element");
-    console.log(element);
     let carComfortVisibility = Array(element.carComfort.length).fill('flex');
     for(let i=0; i<carComfortVisibility.length; i++){
         if(element.carComfort[i]===false){
             carComfortVisibility[i]='none';
         }
     }
-    console.log("carComfortVisibility");
-    console.log(carComfortVisibility);
     let carComfortImages = [snowflake, seat, wifi, noSmoking];
     return(
-        <div className="drivers_block_element driverInfo_background">
+        <div className="drivers_block_element driverInfo_background d-flex flex-row">
             <div className="block_element_left driverInfo_element_left">
                 <div className="driverInfo_left_line" style={{marginBottom: "5%"}}>
                 <div className="block_element_photo">
@@ -112,15 +107,15 @@ const DriverInfo = (props) =>{
                     <Stars value="5.0" commentNumber="10000 отзывов" valueDisplay="none" commentNumberDisplay="none"/>
                     </div>
                     <div className="block_element_infoBlock_bot">
-                    <div className="block_element_infoBlock_element">
+                    <div className="block_element_infoBlock_element d-flex flex-row">
                         <div className="infoString">Возраст:</div>
                         <div className="visibleString">{element.age}</div>
                     </div>
-                    <div className="block_element_infoBlock_element">
+                    <div className="block_element_infoBlock_element  d-flex flex-row">
                         <div className="infoString">Языки:</div>
                         <div className="visibleString">{element.language}</div>
                     </div>
-                    <div className="block_element_infoBlock_element">
+                    <div className="block_element_infoBlock_element  d-flex flex-row">
                         <div className="infoString">За рулём:</div>
                         <div className="visibleString">{element.drivingAge + " лет"}</div>
                     </div>
@@ -135,21 +130,21 @@ const DriverInfo = (props) =>{
                 </div>
             </div>
             <div className="block_element_right driversInfo_element_right">
-              <div className="element_right_line">
+              <div className="d-flex flex-row">
                 <div className="driverInfo_carImage">
-                  <img src={sedan} width="100%" height="auto" alt={"car" + element}></img>
+                  <img src={sedan} height="100%" width="100%"  alt={"car" + element}></img>
                 </div>
-                <div className="tripBlock_carData driverInfo_carData">
+                <div className="tripBlock_carData driverInfo_carData d-flex flex-row">
                     <div className="driverInfo_carBrand">{element.carBrand+","}</div>
                     <div className="driverInfo_fuelType">{element.fuelType}</div>
                     <div className="driverInfo_carProps">{element.carType+", "+element.carCapacity+" места"}</div>
                 </div>
               </div>
-              <div className="element_right_line driverInfo_iconLine">
+              <div className="element_right_line driverInfo_iconLine d-flex flex-row">
               {
                   carComfortVisibility.map((elem,index)=>
                       <div className="driverInfo_iconLine_icon" style={{display:carComfortVisibility[index]}}>
-                        <img src={carComfortImages[index]} width="100%" height="100%" alt=""></img>
+                        <img src={carComfortImages[index]}  height="100%" width="100%" alt=""></img>
                       </div>                 
                   )
               }
@@ -225,44 +220,48 @@ class DriverProfileClass extends React.Component{
         })
     }
     changeSuccessVisibility(value){
-    this.setState({
-        successVisibility: value
-    })
+        this.setState({
+            successVisibility: value
+        })
     }
     render(){
-    console.log("DriverProfile render");
+    //console.log("DriverProfile render");
     let driver = this.props.driversState.drivers[this.props.match.params.id];
-    console.log("this.props.commentState");
-    console.log(this.props.commentState);
+    //console.log("this.props.commentState");
+    //console.log(this.props.commentState);
     return(
         <React.Fragment>
-            <div className = "drivers_top_background">
-                <div className="travelFormBlock">
-                    <StartTravelForm changeTravelVisibility={this.changeTravelVisibility} changeSuccessVisibility={this.changeSuccessVisibility}
-                    travelVisibility={this.state.travelVisibility} successVisibility={this.changeSuccessVisibility}/>
-                    <StartTravelSuccess successVisibility={this.state.successVisibility} changeSuccessVisibility={this.changeSuccessVisibility}/>               
+            <div className = "drivers_top_background col-12">
+                <div className="wrapper d-flex flex-column">
+                    <div className = "drivers_top_block d-flex flex-column">
+                        <div className="travelFormBlock">
+                            <StartTravelForm changeTravelVisibility={this.changeTravelVisibility} changeSuccessVisibility={this.changeSuccessVisibility}
+                            travelVisibility={this.state.travelVisibility} successVisibility={this.changeSuccessVisibility}/>
+                            <StartTravelSuccess successVisibility={this.state.successVisibility} changeSuccessVisibility={this.changeSuccessVisibility}/>               
+                        </div>
+                        <Header colorClass="colorClass" colorClass2="colorClass2" backgroundColorClass="backgroundColorClass"
+                        borderColorClass="borderColorClass" labelColorClass="labelColorClass" type={1}/>
+                        <DriverInfo element={driver} changeCar={this.changeCar} carImageNumber={this.state.carImageNumber} carImages={this.state.carImages}/>
+                        <DriverAdaptedRoute element={driver} date={this.props.storeState.date} cities={this.props.storeState.cities}
+                            travelTime={this.props.driversState.travelTime} travelLength={this.props.driversState.travelLength} goToDrivers={this.goToDrivers}
+                            changeTravelVisibility={this.changeTravelVisibility}
+                        />
+                    </div>
                 </div>
-                <Header colorClass="colorClass" colorClass2="colorClass2" backgroundColorClass="backgroundColorClass"
-                borderColorClass="borderColorClass" labelColorClass="labelColorClass" type={1}/>
-                <DriverInfo element={driver} changeCar={this.changeCar} carImageNumber={this.state.carImageNumber} carImages={this.state.carImages}/>
-                <DriverAdaptedRoute element={driver} date={this.props.storeState.date} cities={this.props.storeState.cities}
-                    travelTime={this.props.driversState.travelTime} travelLength={this.props.driversState.travelLength} goToDrivers={this.goToDrivers}
-                    changeTravelVisibility={this.changeTravelVisibility}
-                />
             </div>
-
-            <div className = "drivers_bottom_background" >
-                <div className = "drivers_body">
-                    <div className="left_body_part">
-                        <DriversProfileComments page={this.state.page} />
-                        <Manipulator number = {this.props.commentState.comments.length} page={this.state.page} elementsNumber={5} setPage={this.setPage}/>
+            <div className="wrapper d-flex flex-column">
+                <div className = "drivers_bottom_background d-flex flex-column" >
+                    <div className = "drivers_body">
+                        <div className="left_body_part">
+                            <DriversProfileComments page={this.state.page} />
+                            <Manipulator number = {this.props.commentState.comments.length} page={this.state.page} elementsNumber={5} setPage={this.setPage}/>
+                        </div>
+                        <div className="right_body_part">
+                            <DriversCommercial/>
+                        </div>
+                        
                     </div>
-                    <div className="right_body_part">
-                        <DriversCommercial/>
-                    </div>
-                    
-                </div>
-                
+                </div>    
             </div>
             <Footer/>
         </React.Fragment>
