b50b7c0657ab963f3d85f31a8595209f12e0278a
Alex <mr.portos>
2019-04-23 13:08:56 +0300

    fix all

--- src/components/driverProfileRegistration/DriverProfileTripSettingsTour.jsx
+++ src/components/driverProfileRegistration/DriverProfileTripSettingsTour.jsx
@@ -498,13 +498,9 @@ class DriverProfileTripSettingsTourClass extends React.Component {
                 <div className="tourBodyElement">
                     <div className="p-0 d-flex  justify-content-xl-start justify-content-lg-start justify-content-md-start justify-content-sm-center justify-content-center flex-wrap col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                         <div style={{ display: this.state.collapse ? "none" : "block" }} onClick={this.toggle} className="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-11 p-2" >
-                            <div className="filledCard d-flex flex-column p-0">
-                                <div className="filledCardImgAdd d-flex justify-content-center align-items-center">
-                                    <div className="filledTourImgAddBg">
-                                        <span />
-                                    </div>
-                                </div>
-                                <div className="cardInformationType d-flex flex-column justify-content-center align-items-center">
+                            <div className="filledTourImgAddBg">
+                                <div className="d-flex flex-column justify-content-center align-items-center">
+                                    <span />
                                     <p>Добавить тур</p>
                                 </div>
                             </div>
