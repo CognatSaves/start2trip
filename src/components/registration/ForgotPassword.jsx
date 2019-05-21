bc1f0b02514341d7a9213e3fc7c09d2306f72ed0
Alex <mr.portos>
2019-05-21 17:11:56 +0300

    fix css forgot-password page

--- src/components/registration/ForgotPassword.jsx
+++ src/components/registration/ForgotPassword.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import Header from '../header/Header';
+import './ForgotPassword.css'
 import requests from '../../config';
 import DriverRefreshIndicator from '../driverProfileRegistration/DriverRefreshIndicator';
-class ForgotPassword extends React.Component{
-    constructor(props){
-        super(props);       
-        this.state={
+class ForgotPassword extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
             email: '',
             isSended: false,
             isRefreshExist: false,
-            isGood: true,
-            isChanged: false
+            isGood: false,
+            isChanged: false,
+            falde: false,
         };
-        
-        this.emailonchange = this.emailonchange.bind(this);
-        this.sendRequest = this.sendRequest.bind(this);
     }
-    emailonchange(value){   
+    emailonchange = (value) => {
         this.setState({
             email: value,
             isChanged: true
         });
     }
-    sendRequest(){
-        if(this.state.isChanged){
+    sendRequest = () => {
+        if (this.state.isChanged) {
             console.log('You want to send that kek');
             console.log(this.state.email);
             this.setState({
@@ -33,67 +32,78 @@ class ForgotPassword extends React.Component{
             })
             let body = JSON.stringify({
                 email: this.state.email,
-                url: requests.frontendAddress+'/reset-password'
+                url: requests.frontendAddress + '/reset-password'
             });
             let that = this;
             fetch(requests.forgotPassword, {
                 method: 'POST', body: body
             })
-            .then(response =>{
-                return response.json();
-            })
-            .then(function (data){
-                console.log('data');
-                console.log(data);
-                if(data.error){
-                    throw Error(data.error.errorMsg);
-                }
-                if(data.message){
-                    if(data.message==="Too many attempts, please try again in a minute."){
-                    //throw Error('не кликой. подумой! а то я сейчас кликну!');
-                        that.props.history.push('/');
+                .then(response => {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log('data');
+                    console.log(data);
+                    if (data.error) {
+                        throw Error(data.error.errorMsg);
                     }
-                }
-                that.setState({
-                    isSended: true,
-                    isGood: true,
-                    isRefreshExist: false
-                });
-            })
-            .catch(function (error){
-                console.log("bad");
-                console.log('An error occurred:', error);
-                that.setState({
-                    isSended: true,
-                    isGood: false,
-                    isRefreshExist: false
-                });
-            })
+                    if (data.message) {
+                        if (data.message === "Too many attempts, please try again in a minute.") {
+                            //throw Error('не кликой. подумой! а то я сейчас кликну!');
+                            that.props.history.push('/');
+                        }
+                    }
+                    that.setState({
+                        isSended: true,
+                        isGood: true,
+                        isRefreshExist: false,
+                        falde: false,
+                    });
+                })
+                .catch(function (error) {
+                    console.log("bad");
+                    console.log('An error occurred:', error);
+                    that.setState({
+                        isSended: true,
+                        isGood: false,
+                        isRefreshExist: false,
+                        falde: true
+                    });
+                })
         }
-        else{
-            alert('Измени хоть что!');
+        else {
+            this.setState({ falde: true })
         }
     }
-    render(){
+    render() {
         console.log('Forgot password render');
         console.log(this.state);
-        return(
+        return (
             <React.Fragment>
-                <Header driver={true} history={this.props.history}/>
-                <DriverRefreshIndicator isRefreshExist={this.state.isRefreshExist} isRefreshing={true} isGoodAnswer={true}/>
-                <div className="d-flex flex-column">
-                    <div>
-                        Здесь вы можете восстановить ваш пароль
-                    </div>
-                    <div>
-                        Введите адрес вашей почты
-                    </div>
-                    <input value={this.state.email} onChange={(e)=>this.emailonchange(e.target.value)}></input>
-                    <button onClick={()=>this.sendRequest()}>Попытать счастья!</button>
-                    {
-                        this.state.isSended ? 
-                        <div>{this.state.isGood ? 'Всё отправлено, ловите письмо' : 'Всё плохо'}</div> :
-                        <React.Fragment/>
+
+                <DriverRefreshIndicator isRefreshExist={this.state.isRefreshExist} isRefreshing={true} isGoodAnswer={true} />
+                <div className="forgotPasswordBody d-flex flex-column align-items-center">
+                    <Header driver={false} history={this.props.history} />
+                    {this.state.isGood ?
+                        <div className="forgotPasswordSuccess forgotPasswordContent d-flex flex-column align-items-center col-md-7 col-11">
+                            <span>Спасибо! Информация для смены пароля выслана на Ваш email.</span>
+                        </div>
+                        :
+                        <div className="forgotPasswordContent d-flex flex-column align-items-center col-md-5 col-11">
+                            <div className="d-flex justify-content-center align-items-center">
+                                <span>Здесь вы можете восстановить ваш пароль</span>
+                            </div>
+                            <input className={this.state.falde ? "forgotPasswordContent-error" : ""} value={this.state.email} placeholder="Введите адрес вашей почты" pattern="" onChange={(e) => { this.emailonchange(e.target.value); this.setState({ falde: false, isSended: false }) }}></input>
+                            <div className="d-flex justify-content-end align-items-center w-100">
+                                {
+                                    this.state.isSended ?
+                                        <div className={this.state.isGood ? "forgotPasswordContent-active" : "forgotPasswordContent-error"}>{this.state.isGood ? 'Отправлено,проверьте почту' : 'Неверная почта'}</div> :
+                                        <div></div>
+                                }
+                                <div className="forgotPasswordBt" onClick={() => this.sendRequest()}><span>Отправить на почту</span></div>
+                            </div>
+                        </div>
+
                     }
                 </div>
 
