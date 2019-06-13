0893a29e2f77ab311915fa9a4fd6ebf7dab9598c
Alex <mr.portos>
2019-06-13 11:12:30 +0300

    fix config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import serverAddress from './serverAddress';
-const frontendAddress = 'http://tripfer.com'; //'http://localhost:3000'; //
+const frontendAddress = 'http://tripfer.com'; //'http://localhost:3000';//
 
 const serverRegistrationRequest = serverAddress + '/auth/local/register';
 const serverAuthorizationRequest = serverAddress + '/auth/local';
@@ -21,14 +21,6 @@ const userTourActivateRequest = serverAddress + '/tours/me/activate'; //запр
 const getLocals = serverAddress + "/getLocals"; //первичный запрос массивов языков и валют
 const getDrivers = serverAddress + "/users/findDrivers"; //запрос на водителей для выбранного маршрута
 const getDriverData = serverAddress + "/users/getDriverData"; //запрос данных водителя с его машиной
-<<
-<< << < HEAD
-const checkPromocode = serverAddress + "/checkPromocode/"; //запрос на проверку промокода
-const createNewTrip = serverAddress + "/createNewTrip"; //запрос на создание новой поездки (без подтверждений)
-const customerConfirmation = serverAddress + '/customerConfirmation'; //запрос на подтверждение заказа пользователем
-const carrierConfirmation = serverAddress + "/carrierConfirmation"; //запрос на подтверждение водителем поездки
-===
-=== =
 const checkPromocode = serverAddress + "/checkPromocode/"; //запрос на проверку промокода
 const createNewTrip = serverAddress + "/createNewTrip"; //запрос на создание новой поездки (без подтверждений)
 const customerConfirmation = serverAddress + '/customerConfirmation'; //запрос на подтверждение заказа пользователем
@@ -37,8 +29,6 @@ const tripStart = serverAddress + "/tripStart"; //функция начала п
 const tripEnd = serverAddress + "/tripEnd"; //функция окончания поездки водителем
 const executeSystemPayment = serverAddress + "/executeSystemPayment"; //выполнить оплату за пользование системой
 const getUserTransactions = serverAddress + "/getUserTransactions"; //обновить таблицу транзакций в админке под выбранный пользователем промежуток времени
->>>
->>> > f67bd5d430938706c2bf878b099d763531eccee2
 
 
 const forgotPassword = serverAddress + "/auth/forgotPassword";
@@ -72,16 +62,10 @@ export default {
     getDriverData: getDriverData,
     checkPromocode: checkPromocode,
     createNewTrip: createNewTrip,
-    <<
-    << << < HEAD
     customerConfirmation: customerConfirmation,
-    carrierConfirmation: carrierConfirmation ===
-        === =
-        customerConfirmation: customerConfirmation,
     carrierConfirmation: carrierConfirmation,
     tripStart: tripStart,
     tripEnd: tripEnd,
     executeSystemPayment: executeSystemPayment,
-    getUserTransactions: getUserTransactions >>>
-        >>> > f67bd5d430938706c2bf878b099d763531eccee2
+    getUserTransactions: getUserTransactions
 };
